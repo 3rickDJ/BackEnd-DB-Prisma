3af40394b053baba93aaf1dcd6afe14b01d7db75
3rick <erick.dejesus1@alumno.buap.mx>
2022-05-14 18:55:22 -0500

    feat: CRUD server feat

--- server.js
+++ server.js
@@ -10,6 +10,50 @@ const prisma = new PrismaClient();
 app.get('/', (req, res) => {
   res.json({message: 'alive'});
 });
+// regrese todos los explorers.
+app.get('/explorers', async (req, res) => {
+  const allExplorers =  await prisma.explorer.findMany({});
+  res.json(allExplorers);
+});
+// regrese el explorer al enviar un ID por query params
+app.get('/explorers/:id', async (req, res) => {
+  const id = req.params.id;
+  const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+  res.json(explorer);
+});
+// crear nuevos explorers.
+app.post('/explorers', async (req, res) => {
+  const explorer = {
+    name: req.body.name,
+    username: req.body.username,
+    mission: req.body.mission
+   };
+  const message = 'Explorer creado.';
+  await prisma.explorer.create({data: explorer});
+  return res.json({message});
+});
+// en el cuál recibirás el ID del explorer a actualizar, y en el cuerpo del request los campos a actualizar, para este caso solo haremos el update del campo mission.
+app.put('/explorers/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+
+	await prisma.explorer.update({
+		where: {
+			id: id
+		},
+		data: {
+			mission: req.body.mission
+		}
+	})
+
+	return res.json({message: "Actualizado correctamente"});
+});
+// eliminar un explorer dado un ID por query params.
+app.delete('/explorers/:id', async (req, res) => {
+	const id = parseInt(req.params.id);
+	await prisma.explorer.delete({where: {id: id}});
+	return res.json({message: "Eliminado correctamente"});
+});
+
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
